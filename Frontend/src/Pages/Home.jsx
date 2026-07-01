5d2867c0c20a5fdd9676f56aec0efbe0b9b18cb2
vishalkumar2024 <vkumarsoni30@gmail.com>
2026-07-01 19:56:43 +0530

    minor change

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -80,7 +80,7 @@ function Home() {
 
     if (loading) {
         return (
-            <div className="flex flex-col items-center justify-center  pb-50  min-h-screen gap-4">
+            <div className="bg-[#0d1117] flex flex-col items-center justify-center  pb-50  min-h-screen gap-4">
                 <div className="w-16 h-16 border-4 border-cyan-500 border-t-transparent rounded-full animate-spin"></div>
                 <p className="text-gray-400 text-sm">Loading....</p>
             </div>
